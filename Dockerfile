243490f8c3eae5106b27e2ec97a4eb09b68ef090
huyhuy17.xyz <huyhuy17@gmail.com>
2020-04-27 11:39:09 +0700

    cập nhật thêm docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,15 @@
 FROM python:3
 
+# Set the working directory.
+RUN  mkdir /usr/src/app
+WORKDIR /usr/src/app
+COPY . .
+
+#install requirement
+RUN  pip install -r requirements.txt
+
+
+
 ENV HOST=0.0.0.0
 ENV PORT=8888
 
--- main.py
+++ main.py
@@ -6,15 +6,19 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
         self.write("<br>wtf")
+        self.write("<h1>wtf</h1>")
 
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-    ])
+    ],
+        debug=True
+    )
 
 
 if __name__ == "__main__":
     app = make_app()
+    print("App has start at 8888")
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
